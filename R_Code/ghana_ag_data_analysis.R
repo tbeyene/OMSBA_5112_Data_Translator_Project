4f29e7e8ca17e1f4d84010a0f0e91cf341e6fe2e
John <55668297+Duong-Nguyen73@users.noreply.github.com>
2019-12-01 16:52:22 -0800

    getting started

---
+++ R_Code/ghana_ag_data_analysis.R
@@ -0,0 +1,27 @@
+# Data translator project
+
+# Ghana Living Standards Survey
+
+#Business Problem ------------------------------------------
+
+# The ACME corporation is considering moving 
+# into agricultural inputs in Ghana. 
+# To get an idea of whom to target for their sales efforts 
+# they have hired you to analyze what determines 
+# agricultural profit there. They are especially interested 
+# in what effect household educational attainment and 
+# the characteristics of the local area has on profit, 
+# but if you find other results of interest, they would like
+# to hear those as well. They have left the choice of 
+# explanatory variables and variable definitions to you, 
+# but to be able to compare across regions they want at
+# least one specification that examines profit per area unit
+
+
+# Import lib and data----------------------------------------
+
+library(tidyverse)
+library(haven)
+
+ds1 <- read_dta('Raw_Data/glss4/sec0a.dta')
+# variables, aggricutural profits with education with characteristics of local area

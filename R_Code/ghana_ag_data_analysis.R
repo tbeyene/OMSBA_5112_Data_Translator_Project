e2a5ec52f8388703de545f25f28fbfcdfd0084d9
Eric Hestekin <ehestekin@seattleu.edu>
2019-12-04 19:55:10 -0800

    Data files loaded

--- R_Code/ghana_ag_data_analysis.R
+++ R_Code/ghana_ag_data_analysis.R
@@ -23,4 +23,31 @@
 library(tidyverse)
 library(haven)
 
-test <- read_dta('Raw_Data/Unzipped Data/sec0a.dta')
+#some issues with data need stata 13 read function
+#install.packages('readstata13')
+
+library(readstata13)
+
+#array of all .dta files in dataset (not using this section)----
+file_list <- list.files('Raw_Data/Unzipped Data/', pattern = '\\.dta*', recursive = TRUE)
+
+#read in file from list
+test <- read_dta(paste('Raw_Data/Unzipped Data/' , file_list[1], sep = ''))
+
+#household and location info------------------------------------
+id_df         <- read.dta13('Raw_Data/Unzipped Data/sec0a.dta')
+education_df  <- read.dta13('Raw_Data/Unzipped Data/sec2a.dta')
+land_chars_df <- read.dta13('Raw_Data/Unzipped Data/sec8a1.dta')
+
+#financials-----------------------------------------------------
+ag_profit_df    <- read.dta13('Raw_Data/Unzipped Data/aggregates/agg2.dta')
+costs_land_df   <- read.dta13('Raw_Data/Unzipped Data/aggregates/exp3.dta')
+costs_crops_df  <- read.dta13('Raw_Data/Unzipped Data/aggregates/exp4.dta')
+costs_livestock <- read.dta13('Raw_Data/Unzipped Data/aggregates/exp5.dta')
+
+#community info-------------------------------------------------
+community_df        <- read.dta13('Raw_Data/Unzipped Data/community/cs2.dta')
+community_health_df <- read.dta13('Raw_Data/Unzipped Data/community/cs4b.dta')
+community_ag_df     <- read.dta13('Raw_Data/Unzipped Data/community/cs5b.dta')
+
+
